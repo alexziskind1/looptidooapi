54e5695cabd21592c5b7ac383abb51c72eac06a5
Alexander Ziskind <alex@nuvious.com>
2017-10-19 10:44:00 -0400

    tasks, items, CORS, yada yada yada

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import * as http from 'http';
 import { Express, Router, Request, Response } from "express";
 
 import * as mockgen from './app/data/mock-data-generator';
-import { PtAuthToken } from "./app/shared/models/domain-models";
+import { PtAuthToken, PtItem, PtTask } from "./app/shared/models/domain-models";
 import { newGuid } from "./app/util/guid";
 
 const port = 8080;
@@ -43,6 +43,7 @@ app.use(express.static(__dirname + '/app'));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -128,6 +129,116 @@ router.get('/openItems', (req: Request, res: Response) => {
 router.get('/closedItems', (req: Request, res: Response) => {
     let filteredItems = currentPtItems.filter(i => i.status === 3 /*closed*/);
     res.json(filteredItems);
+});
+
+router.get('/item/:id', (req: Request, res: Response) => {
+    const itemId = parseInt(req.params.id);
+    const foundItem = currentPtItems.find(i => i.id === itemId && i.dateDeleted === undefined);
+
+    let found = false;
+    if (foundItem) {
+        found = true;
+    }
+
+    if (!found) {
+        res.status(404);
+        res.json(null);
+    } else {
+        res.json(foundItem);
+    }
+});
+
+router.post('/item', (req: Request, res: Response) => {
+    if (req.body) {
+        if (req.body.item) {
+            const newItem = <PtItem>req.body.item;
+            const newItemId = currentPtItems.length > 0 ? (Math.max(...currentPtItems.map(i => i.id))) + 1 : 1;
+            newItem.id = newItemId;
+            const newItems = [...currentPtItems, newItem];
+            currentPtItems = newItems;
+            res.json(newItem);
+        } else {
+            res.json(null);
+        }
+    }
+});
+
+router.post('/task', (req: Request, res: Response) => {
+    if (req.body) {
+        if (req.body.task && req.body.itemId) {
+
+            const newTask = <PtTask>req.body.task;
+            const itemId = parseInt(req.body.itemId);
+
+            const foundItem = currentPtItems.find(i => i.id === itemId && i.dateDeleted === undefined);
+
+            newTask.id = foundItem.tasks.length > 0 ? (Math.max(...(foundItem.tasks.map(t => t.id)))) + 1 : 1;
+
+            const updatedTasks = [newTask, ...foundItem.tasks];
+
+            const updatedItem = Object.assign({}, foundItem, { tasks: updatedTasks });
+
+            const updatedItems = currentPtItems.map(i => {
+                if (i.id === itemId) { return updatedItem; } else { return i; }
+            });
+
+            /*
+            const newItems = currentPtItems.map(i => {
+                if (i.id === itemId) {
+                    const newTaskId = i.tasks.length > 0 ? (Math.max(...(i.tasks.map(t => t.id)))) + 1 : 1;
+                    newTask.id = newTaskId;
+                    i.tasks = [newTask, ...i.tasks];
+                    return i;
+                } else {
+                    return i;
+                }
+            }); 
+            */
+
+            currentPtItems = updatedItems;
+
+            res.json(newTask);
+        } else {
+            res.json(null);
+        }
+    }
+});
+
+router.put('/task/:id', (req: Request, res: Response) => {
+    const taskId = req.params.id;
+
+    if (req.body) {
+        if (req.body.task && req.body.itemId) {
+            let found = false;
+            const modifiedTask = <PtTask>req.body.task;
+            const itemId = parseInt(req.body.itemId);
+
+            const foundItem = currentPtItems.find(i => i.id === itemId && i.dateDeleted === undefined);
+
+            const updatedTasks = foundItem.tasks.map(t => {
+                if (t.id === modifiedTask.id) {
+                    found = true;
+                    return modifiedTask;
+                } else { return t; }
+            });
+
+            const updatedItem = Object.assign({}, foundItem, { tasks: updatedTasks });
+
+            const updatedItems = currentPtItems.map(i => {
+                if (i.id === itemId) { return updatedItem; } else { return i; }
+            });
+
+            currentPtItems = updatedItems;
+
+            if (!found) {
+                res.status(404);
+            }
+            res.json({
+                id: taskId,
+                result: modifiedTask
+            });
+        }
+    }
 });
 
 router.get('/photo/:id', (req: Request, res: Response) => {
